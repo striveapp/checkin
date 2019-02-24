a371134e51ea98198461d5d8244f522ca61f0504
Stefano Saitta <stefano.saitta@thoughtworks.com>
2019-02-24 23:32:40 +0100

    Setup login with firebase for both IOS & Android

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:checkin/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('It should check that login button is present', (WidgetTester tester) async {
     await tester.pumpWidget(App());
+    expect(find.text('Login'), findsOneWidget);
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('It should check that Team button is present', (WidgetTester tester) async {
+    await tester.pumpWidget(App());
+    expect(find.text('Teams'), findsOneWidget);
   });
 }
