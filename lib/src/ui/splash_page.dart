cf13d6a8d265fad8064f79f92f9f41a02ac4832d
Stefano Saitta <stefano.saitta92@gmail.com>
2019-05-15 22:33:48 +0200

    style splash page

--- lib/src/ui/splash_page.dart
+++ lib/src/ui/splash_page.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'loading_indicator.dart';
+
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Splash Screen'),
-      ),
-    );
+        body: new Container(
+      alignment: Alignment.center,
+      decoration: new BoxDecoration(
+          image: new DecorationImage(
+        image: new AssetImage("assets/images/trianglify.png"),
+        fit: BoxFit.cover,
+      )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: new EdgeInsets.symmetric(vertical: 50),
+                child: Image.asset(
+                  "assets/images/logo.png",
+                  width: 150.0,
+                ),
+              ),
+              LoadingIndicator()
+            ],
+          ),
+    ));
   }
 }
