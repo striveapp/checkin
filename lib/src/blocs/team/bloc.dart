c224f0f13b0288f1042a7ef4b675d1b94e3f35ff
dcapozzi <davide.capozzi@objectway.it>
2019-10-16 21:01:38 +0200

    [skip ci] delete team

--- lib/src/blocs/team/bloc.dart
+++
@@ -1,3 +0,0 @@
-export 'package:checkin/src/blocs/team/team_bloc.dart';
-export 'package:checkin/src/blocs/team/team_event.dart';
-export 'package:checkin/src/blocs/team/team_state.dart';
--- lib/src/blocs/team/team_event.dart
+++
@@ -1,8 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-abstract class TeamEvent extends Equatable {}
-
-class Fetch extends TeamEvent {
-  @override
-  String toString() => 'Fetch';
-}
