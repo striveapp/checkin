45f98e17e1ed73741f613b949d5dee6ea5cd6ec6
Stefano Saitta <stefano.saitta92@gmail.com>
2020-05-05 12:36:07 +0200

    remove toggle and activate the master stats page for all

--- lib/src/ui/components/stats/stats_body.dart
+++ lib/src/ui/components/stats/stats_body.dart
@@ -3,7 +3,6 @@ import 'package:checkin/src/models/master.dart';
 import 'package:checkin/src/ui/components/loading_indicator.dart';
 import 'package:checkin/src/ui/pages/lessons_stats_page.dart';
 import 'package:checkin/src/ui/pages/user_stats_page.dart';
-import 'package:checkin/src/util/debug_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,7 +12,7 @@ class StatsBody extends StatelessWidget {
     return BlocBuilder<ProfileBloc, ProfileState>(
       builder: (BuildContext context, ProfileState state) {
         if (state is ProfileSuccess) {
-          if (state.profileUser.isOwner && isInDebugMode) {
+          if (state.profileUser.isOwner) {
             return LessonsStatsPage(master: Master.fromUser(state.profileUser));
           }
 
