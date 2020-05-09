69727bb73bfad79285920216f09e3c1ca1b6a51c
Stefano Saitta <stefano.saitta92@gmail.com>
2020-05-09 19:22:17 +0200

    [skip ci] adapt production data for aranha gym

--- firebase/functions/src/admin/imports/data/subscription_plans.ts
+++ firebase/functions/src/admin/imports/data/subscription_plans.ts
@@ -2,27 +2,12 @@ const subscriptionPlans : any = {
     "aranha": {
         "subscription_plans": [
             {
-                "code": "plan_GRWHb4jeVX66Lq",
+                "code": "plan_GgHOhPe89W6J2M",
                 "currency": "EUR",
-                "description": "Some rich shit",
-                "name": "Wow Plan",
-                "price": 50000,
-            },
-            {
-                "code": "plan_GREmgLjDaw1LsE",
-                "currency": "EUR",
-                "description": "Some thing",
                 "interval": "Monthly",
-                "name": "Full Plan",
-                "price": 7500,
-            },
-            {
-                "code": "plan_GREnni0w3EtKm8",
-                "currency": "EUR",
-                "description": "Some other things",
-                "interval": "Monthly",
-                "name": "Starter Plan",
-                "price": 6500,
+                "description": "All classes",
+                "name": "Full Month",
+                "price": 7000,
             },
         ]
     },
@@ -33,7 +18,7 @@ const subscriptionPlans : any = {
                 "currency": "EUR",
                 "description": "Some test shit",
                 "name": "Wow Test Plan",
-                "price": 5000,
+                "price": 50000,
             },
             {
                 "code": "plan_GREmgLjDaw1LsE",
