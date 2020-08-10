db87c1fc3f5524b40b5a3761c427bbf38fc27bb5
dcapozzi <davide.capozzi@objectway.it>
2020-08-11 00:36:25 +0200

    update to new theme with device based dark mode

--- lib/src/blocs/theme/theme_bloc.dart
+++ lib/src/blocs/theme/theme_bloc.dart
@@ -1,11 +1,21 @@
 import 'dart:async';
+import 'dart:ui';
 import 'package:bloc/bloc.dart';
 import 'package:checkin/src/themes/theme.dart';
+import 'package:flutter/scheduler.dart';
 import './bloc.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
   @override
-  ThemeState get initialState => ThemeState.appTheme(themeData: theme[ThemeType.Light]);
+  ThemeState get initialState {
+    var brightness = SchedulerBinding.instance.window.platformBrightness;
+    bool darkModeOn = brightness == Brightness.dark;
+    if (darkModeOn) {
+      return ThemeState.appTheme(themeData: theme[ThemeType.Dark]);
+    }
+
+    return ThemeState.appTheme(themeData: theme[ThemeType.NewLight]);
+  }
 
   @override
   Stream<ThemeState> mapEventToState(
