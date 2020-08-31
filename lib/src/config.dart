6009b5b339018b482601408f7435e115e11e0378
Stefano Saitta <stefano.saitta92@gmail.com>
2020-08-31 23:22:22 +0200

    change domains for admin-app in client

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -7,8 +7,8 @@ const int TOTAL_MAT_TIME_YEAR=300;
 
 const TEST_STORAGE_BUCKET = 'gs://checkin-test-fba3d.appspot.com';
 const STORAGE_BUCKET = 'gs://checkin-b7e8d.appspot.com';
-const TEST_CLOUD_FUNCTIONS_DOMAIN = "https://checkin-test-fba3d.firebaseapp.com";
-const CLOUD_FUNCTIONS_DOMAIN = "https://checkin-b7e8d.firebaseapp.com";
+const TEST_CLOUD_FUNCTIONS_DOMAIN = "https://admin-console-test-fba3d.firebaseapp.com";
+const CLOUD_FUNCTIONS_DOMAIN = "https://admin-console-b7e8d.firebaseapp.com";
 
 //TODO: change this with something stored in our servers
 const DEFAULT_USER_IMAGE_URL = "https://firebasestorage.googleapis.com/v0/b/checkin-b7e8d.appspot.com/o/default_pic.png?alt=media&token=3028f80c-8009-478e-a631-ff9e39f1b48a";
