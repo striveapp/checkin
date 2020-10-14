50e4ba86eccfcd4b4711091cc7ac47417c7c4c65
Stefano Saitta <stefano.saitta92@gmail.com>
2020-10-15 00:20:52 +0200

    add property to UserImage that adds a border

--- lib/src/ui/components/user_image.dart
+++ lib/src/ui/components/user_image.dart
@@ -6,25 +6,34 @@ class UserImage extends StatelessWidget {
   final double circularRadius;
   final double width;
   final double height;
+  final bool withBorder;
 
-  UserImage(
-      {Key key,
-      @required this.userImage,
-      this.circularRadius = 50,
-      this.width = 50,
-      this.height = 50})
-      : super(key: key);
+  UserImage({
+    Key key,
+    @required this.userImage,
+    this.circularRadius = 50,
+    this.width = 50,
+    this.height = 50,
+    this.withBorder = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(circularRadius),
-      child: CachedNetworkImage(
-        imageUrl: userImage,
-        width: width,
-        height: height,
-        fit: BoxFit.fill,
-        errorWidget: (context, url, error) => Icon(Icons.error),
+    return Container(
+      padding: this.withBorder ? EdgeInsets.all(1.5) : null,
+      decoration: BoxDecoration(
+          border: Border.all(color: this.withBorder ? Theme.of(context).accentColor : Colors.transparent, width: 1.5),
+          borderRadius: BorderRadius.circular(circularRadius)
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(circularRadius),
+        child: CachedNetworkImage(
+          imageUrl: userImage,
+          width: width,
+          height: height,
+          fit: BoxFit.fill,
+          errorWidget: (context, url, error) => Icon(Icons.error),
+        ),
       ),
     );
   }
