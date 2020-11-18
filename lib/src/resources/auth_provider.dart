e899faa8785b0480a02053cf95d587cf758101b7
dcapozzi <davide.capozzi@objectway.it>
2020-11-18 23:16:36 +0100

    fix logout, now really logs out of google

--- lib/src/resources/auth_provider.dart
+++ lib/src/resources/auth_provider.dart
@@ -103,7 +103,8 @@ class AuthProvider implements AuthRepository {
   Future<void> signOut() async {
     return Future.wait([
       _firebaseAuth.signOut(),
-      _googleSignIn.signOut(),
+      _googleSignIn.disconnect(),
+
     ]);
   }
 
