0a8091f7d379af754cf6693f1d2d979bb89866f2
Stefano Saitta <stefano.saitta92@gmail.com>
2021-01-04 19:17:31 +0100

    change back acceptAllV2 to acceptAll

--- lib/src/api/lesson_api.dart
+++ lib/src/api/lesson_api.dart
@@ -3,17 +3,18 @@ import 'package:flutter/material.dart';
 import 'api.dart';
 
 class LessonApi {
-  Future<void> acceptAll(String gymId, String lessonId, String lessonDate) async {
+  Future<void> acceptAll(
+      String gymId, String lessonId, String lessonDate) async {
     var parameters = {
       'lessonId': lessonId,
       'lessonDate': lessonDate,
       'gymId': gymId
     };
 
-    await Api.call(functionName: "acceptAllV2", parameters: parameters)
-        .then((_) => {
-      debugPrint("User accepted from lesson with id [$lessonDate,$lessonId]")
-    });
+    await Api.call(functionName: "acceptAll", parameters: parameters).then(
+        (_) => {
+              debugPrint(
+                  "User accepted from lesson with id [$lessonDate,$lessonId]")
+            });
   }
 }
-
