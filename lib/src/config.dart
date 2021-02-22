a224caa64c6b3e6f114a6b7bcde33dddd205af8d
Stefano Saitta <stefano.saitta92@gmail.com>
2021-02-22 20:33:46 +0100

    change logo for default gyms

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -13,6 +13,6 @@ const CLOUD_FUNCTIONS_DOMAIN = "https://admin-console-b7e8d.firebaseapp.com";
 const DEFAULT_USER_IMAGE_URL =
     "https://firebasestorage.googleapis.com/v0/b/checkin-b7e8d.appspot.com/o/default_pic.png?alt=media&token=3028f80c-8009-478e-a631-ff9e39f1b48a";
 
-// todo rebranding
+//TODO: Is better to place this in the backend
 const DEFAULT_GYM_IMAGE_URL =
-    "https://firebasestorage.googleapis.com/v0/b/checkin-b7e8d.appspot.com/o/foreground_logo.png?alt=media&token=0fadebe1-b8d9-4434-9604-d0f5d4ca78a5";
+    "https://firebasestorage.googleapis.com/v0/b/checkin-b7e8d.appspot.com/o/adaptive_icon.png?alt=media&token=3094b170-c21c-4940-9e61-6f3b14434752";
