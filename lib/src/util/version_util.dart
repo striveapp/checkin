2aecfe91a39425a0325b3c7927a2650bb6c4ed3b
Stefano Saitta <stefano.saitta92@gmail.com>
2021-02-28 23:45:41 +0100

    get current version return always 1.0.0 when in dev

--- lib/src/util/version_util.dart
+++ lib/src/util/version_util.dart
@@ -1,8 +1,13 @@
 import 'package:package_info/package_info.dart';
 import 'package:pub_semver/pub_semver.dart';
 
+import 'debug_util.dart';
+
 class VersionUtil {
   Future<Version> getCurrentVersion() async {
+    if (isInDebugMode) {
+      return Version(1, 0, 0);
+    }
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
     Version currentVersion = Version.parse(packageInfo.version);
     return currentVersion;
